a53d93363ed34e85303e3384bbf3dfef50092b41
Sangram Thapa <thapasangram64@gmail.com>
2025-03-07 12:00:39 +0545

    added

--- src/protected/Register.jsx
+++ src/protected/Register.jsx
@@ -1,5 +1,47 @@
+import { Button, Form, Input } from "antd";
+
 function Register() {
-  return <div>Register</div>;
+  const submit = (values) => {
+    console.log(values);
+  };
+  return (
+    <div>
+      <Form onFinish={submit} layout="vertical">
+        <Form.Item
+          label="Name"
+          name="user_name"
+          rules={[{ required: true, message: "Please input your name" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Phone"
+          name="phone_number"
+          rules={[{ required: true, message: "Please input your password" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[{ required: true, message: "Please input your email" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Button type="primary" htmlType="submit">
+          Register
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
 export default Register;
